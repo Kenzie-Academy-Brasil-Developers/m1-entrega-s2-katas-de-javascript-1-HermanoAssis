47fa926560528f557e3097454aeec0b870130afa
hermano <hermano_assis@hotmail.com>
2022-06-27 10:47:58 -0300

    katas atualizado

--- katas1.js
+++ katas1.js
@@ -1,6 +1,6 @@
 function oneThroughTwenty() {
   let meuRetorno = [];
-  for(let counter = 1; counter = 20; counter++) {
+  for(let counter = 1; counter <= 20; counter++) {
   meuRetorno.push(counter)
   }
   return meuRetorno;
@@ -10,7 +10,7 @@ console.log(oneThroughTwenty())
 
 function evensToTwenty() {
   let numerosPares = []
-  for(let pares = 0; pares < 20; pares+=2){
+  for(let pares = 0; pares <= 20; pares+=2){
     numerosPares.push(pares);
   }
   return numerosPares;
@@ -20,7 +20,7 @@ console.log(evensToTwenty())
 
 function oddsToTwenty() {
    let numerosImpares = []
-   for(let impares = 1; impares < 19; impares++){
+   for(let impares = 1; impares <= 19; impares++){
     if(impares % 2 == 1){
       numerosImpares.push(impares);
     }
@@ -32,7 +32,7 @@ console.log(oddsToTwenty())
 
 function multiplesOfFive() {
   let multiplos = []
-  for(let i = 5; i < 100; i+=5){
+  for(let i = 5; i <= 100; i+=5){
     multiplos.push(i);
   }
   return multiplos;
@@ -42,7 +42,7 @@ console.log(multiplesOfFive())
 
 function squareNumbers() {
   let numeroQuadrado = []
-  for(let numQuadrado = 1; numQuadrado*numQuadrado < 100; numQuadrado+=1){
+  for(let numQuadrado = 1; numQuadrado*numQuadrado <= 100; numQuadrado+=1){
     numeroQuadrado.push(numQuadrado*numQuadrado)
   }
   return numeroQuadrado
@@ -52,7 +52,7 @@ console.log(squareNumbers())
 
 function countingBackwards() {
   let contandoDeTras = []
-  for( let deTras = 20; deTras > 1; deTras--){
+  for( let deTras = 20; deTras >= 1; deTras--){
     contandoDeTras.push(deTras)
   }
   return contandoDeTras
@@ -62,9 +62,9 @@ console.log(countingBackwards())
 
 function evenNumbersBackwards() {
   let numerosParesTras = []
-  for(let paresTras = 20; paresTras > 2; paresTras--){
-    if(deTras % 2 == 0){
-      numerosParesTras.push(ParesTras)
+  for(let paresTras = 20; paresTras >= 2; paresTras--){
+    if(paresTras % 2 == 0){
+      numerosParesTras.push(paresTras)
     }
   }
   return numerosParesTras
@@ -74,7 +74,7 @@ console.log(evenNumbersBackwards())
 
 function oddNumbersBackwards() {
   let numerosImparesTras = []
-  for(let imparesTras = 19; imparesTras > 1; imparesTras--){
+  for(let imparesTras = 19; imparesTras >= 1; imparesTras--){
     if(imparesTras % 2 == 1){
       numerosImparesTras.push(imparesTras)
     }
@@ -86,7 +86,7 @@ console.log(oddNumbersBackwards())
 
 function multiplesOfFiveBackwards() {
   let numerosMultiplosTras = []
-  for(let multiplosTras = 100; multiplosTras > 1; multiplosTras-=2){
+  for(let multiplosTras = 100; multiplosTras >= 1; multiplosTras-=5){
     numerosMultiplosTras.push(multiplosTras)
   }
   return numerosMultiplosTras
@@ -96,8 +96,10 @@ console.log(multiplesOfFiveBackwards())
 
 function squareNumbersBackwards() {
   let numQuadradoTras = []
-  for(let numQuadrado = 100; numQuadrado*numQuadrado > 1; numQuadrado-=1){
-    numQuadradoTras.push(numQuadrado*numQuadrado)
+  for(let numberQuadrado = 100; numberQuadrado*numberQuadrado >= 1; numberQuadrado -=1){
+    if(numberQuadrado*numberQuadrado <= 100){
+      numQuadradoTras.push(numberQuadrado*numberQuadrado)
+    }
   }
   return numQuadradoTras
 }
